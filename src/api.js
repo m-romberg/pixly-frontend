ffa191216544b0389c7616419bf8207cf45fbdfb
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-05-05 12:11:53 -0700

    change url for production:

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 // const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
-const BASE_URL = "http://127.0.0.1:5001";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 /** API Class.
  *
  * Static class tying together methods used to get/send to to the API.
